b4bc8c4db93aa681dbbadd80c071f9aca3e8c5f7
nikola djordjevic <nikola.dj.87@gmail.com>
2021-04-12 02:01:53 +0200

    user preview restyle

--- src/components/ZanatlijaProfile.js
+++ src/components/ZanatlijaProfile.js
@@ -15,7 +15,41 @@ function ZanatlijaProfile(props) {
         <div className="zanatlijaProfile__wrapper">
             {/* This needs to be separated component after completing users information  */}
             {props.userPreview.length > 0 && <div className="zanatlijeProfile__content">
-                <div className="userProfile__contactAndStats standardShadowBox">
+                <div className="userProfileInformation__wrapper standardShadowBox">
+                    <h3 className="userProfileInformation--name">{user.userName}</h3>
+                    <div className="userProfileInformation__content">
+                        <div className="userProfileInformation__card">
+                            <h4>EMAIL</h4>
+                            <p>{user.email}</p>
+                        </div>
+                        <div className="userProfileInformation__card">
+                            <h4>TELEFON</h4>
+                            <p>{user.userPhoneNumber}</p>
+                        </div>
+                        <div className="userProfileInformation__card">
+                            <h4>KREDIT</h4>
+                            <p>{user.userCredit}</p>
+                        </div>
+                        <div className="userProfileInformation__card">
+                            <h4>STATUS</h4>
+                            <p>{user.userStatus}</p>
+                        </div>
+                        <div className="userProfileInformation__card">
+                            <h4>ADRESA</h4>
+                            <p>{user.userAddress}</p>
+                        </div>
+                        <div className="userProfileInformation__card">
+                            <h4>TEMA</h4>
+                            <p>{user.userTheme}</p>
+                        </div>
+
+                    </div>
+                    <div className="userProfileInformation__card">
+                        <h4>O MENI</h4>
+                        <p>{user.aboutMe}</p>
+                    </div>
+                </div>
+                {/* <div className="userProfile__contactAndStats standardShadowBox">
                     <div className="userProfile__contactAndStats--name">
                         <h3>{user.userName}</h3>
                     </div>
@@ -39,7 +73,7 @@ function ZanatlijaProfile(props) {
                         <p className="userProfile__contactAndStats--grade--icon ">Ocena</p>
                         <p className="userProfile__contactAndStats--grade--text ">10</p>
                     </div>
-                </div>
+                </div> */}
                 <div className="usersServices__wrapper">
                     <div className="usersServices__content">
                         {props.userPreview.map(service => {
