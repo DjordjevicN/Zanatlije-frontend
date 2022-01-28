33619ab64c6ab1476e40ec757e7bf1d324fd121b
nikola <nikola.dj.87@gmail.com>
2022-01-28 16:04:42 +0100

    update to typescript

--- src/components/Proposals.js
+++ src/components/Proposals.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import { BsPeopleCircle } from "react-icons/bs";
+import React from "react";
+// import { BsPeopleCircle } from "react-icons/bs";
 // @@ proposal card component part of inbox (singleTaskPage)
 function Proposals({ proposal }) {
-    return (
-        <div className="proposals__wrapper standardShadowBox">
-            <div className="proposals__content">
-                <div className="proposal__msg--sender">
-                    <BsPeopleCircle className='proposal__msg--icon' />
-                    <h3>{proposal.proposalFromName}</h3>
-                </div>
-                <div className="proposal__msg--body">
-                    <p>{proposal.proposalInitMessage}</p>
-                </div>
-                <div className="proposal__msg--priceOffer">
-                    <p>{`Ponudjena cena: ${proposal.proposalPrice}`}</p>
-                </div>
-            </div>
+  return (
+    <div className="proposals__wrapper standardShadowBox">
+      <div className="proposals__content">
+        <div className="proposal__msg--sender">
+          {/* <BsPeopleCircle className='proposal__msg--icon' /> */}
+          <h3>{proposal.proposalFromName}</h3>
         </div>
-    );
+        <div className="proposal__msg--body">
+          <p>{proposal.proposalInitMessage}</p>
+        </div>
+        <div className="proposal__msg--priceOffer">
+          <p>{`Ponudjena cena: ${proposal.proposalPrice}`}</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Proposals;
---
+++ src/index.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, combineReducers } from "redux";
+import thunk from "redux-thunk";
+import initState from "./store/reducers/initReducer";
+import userReducer from "./store/reducers/userReducer";
+import globalReducer from "./store/reducers/globalReducer";
+
+const rootReducer = combineReducers({
+  initState,
+  userReducer,
+  globalReducer,
+});
+
+// @@ Comment first and second and uncomment third before Build
+// **********
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+const store = createStore(rootReducer, composeEnhancer(applyMiddleware(thunk)));
+// const store = createStore(rootReducer, applyMiddleware(thunk))
+// ********
+
+ReactDOM.render(
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
+  document.getElementById("root")
+);
